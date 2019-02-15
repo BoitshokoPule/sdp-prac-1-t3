7014f575fd48c41dc5e493407278f425f7e94402
LEECRAZYLOVE <crazedoutlee@gmail.com>
2019-02-15 10:04:20 +0200

    Finished the first problem in euler thing

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -10,6 +10,18 @@ namespace Euler
     {
         static void Main(string[] args)
         {
+            //Problem 1: Find the sum of all the multiples of 3 or 5 below 1000.
+            int i = 0; //Counter
+            int sum = 0;
+            while (1 < 1000)
+            {
+                if (i % 3 == 0 || i % 5 == 0) //Checking if the current number is a multiple of 3 or 5
+                {
+                    sum += i;
+                }
+                i++;
+            }
+            Console.WriteLine($"Problem 1: sum is {sum}");
         }
     }
 }
